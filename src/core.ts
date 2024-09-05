6a24ff5e653112cf53938c3097e3539a3229c2c2
nyable <loser@88.com>
2024-09-05 10:40:08 +0800

    remove copy button and fix linenum

--- src/core.ts
+++ src/core.ts
@@ -1,13 +1,11 @@
-import { MarkdownPostProcessorContext, Menu } from "obsidian";
-import CodeBlockCopyPlugin from "./main";
+import { MarkdownPostProcessorContext, MarkdownView, Menu } from 'obsidian';
+import CodeBlockEnhancer from './main';
 
-const DEFAULT_LANG_ATTR = 'language-text'
-const DEFAULT_LANG = ''
-const LANG_REG = /^language-/
-const LINE_SPLIT_MARK = '\n'
-
-const SVG_COPY = parseToSVG('<?xml version="1.0" encoding="utf-8"?> <svg height="16" width="16" viewBox="0 0 16 16" version="1.1" data-view-component="true" class="copy" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z"></path></svg>')
-const SVG_SUCCESS = parseToSVG('<?xml version="1.0" encoding="utf-8"?> <svg height="16" width="16" viewBox="0 0 16 16" version="1.1" data-view-component="true" class="copy-success" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>')
+const DEFAULT_LANG_ATTR = 'language-text';
+const DEFAULT_LANG = '';
+const LANG_REG = /^language-/;
+const LINE_SPLIT_MARK = '\n';
+const FILE_OBSERVER_CACHE: IntersectionObserver[] = [];
 
 interface CodeBlockMeta {
   /**
@@ -33,87 +31,99 @@ interface CodeBlockMeta {
   contentList: string[];
 }
 
-export function enhanceCodeBlock (el: HTMLElement, ctx: MarkdownPostProcessorContext, plugin: CodeBlockCopyPlugin) {
-  const code: HTMLElement = el.querySelector('pre > code')
+export function enhanceCodeBlock(
+  el: HTMLElement,
+  ctx: MarkdownPostProcessorContext,
+  plugin: CodeBlockEnhancer
+) {
+  const view = plugin.app.workspace.getActiveViewOfType(MarkdownView);
+  const code: HTMLElement = el.querySelector('pre > code');
   // only change pre>code
   if (!code) {
-    return
+    return;
   }
-  let lang = DEFAULT_LANG
+  let lang = DEFAULT_LANG;
   // return when lang is in exclude list
-  if (plugin.settings.excludeLangs.some(eLangName => code.classList.contains(`language-${eLangName}`))) {
-    return
+  if (
+    plugin.settings.excludeLangs.some((eLangName) =>
+      code.classList.contains(`language-${eLangName}`)
+    )
+  ) {
+    return;
   }
   code.classList.forEach((value, key, parent) => {
     if (LANG_REG.test(value)) {
-      lang = value.replace('language-', '')
-      return
+      lang = value.replace('language-', '');
+      return;
     }
-  })
-  const pre = code.parentElement
-  const div = pre.parentElement
+  });
+  const pre = code.parentElement;
+  const div = pre.parentElement;
   // Add default language style when lang is empty
   if (!code.classList.toString().contains('language-')) {
-    pre.classList.add(DEFAULT_LANG_ATTR)
+    pre.classList.add(DEFAULT_LANG_ATTR);
   }
-  // Ignore already has copy button 
+  // Ignore already has copy button
   if (pre.querySelector('div.code-block-copy-button')) {
     return;
   }
   // let div position: relative;
-  div.classList.add('code-block-wrap')
+  div.classList.add('code-block-wrap');
   /* const { lineStart, lineEnd } = ctx.getSectionInfo(el)
   const lineSize = lineEnd - lineStart - 1 */
-  const contentList: string[] = code.textContent.split(LINE_SPLIT_MARK)
-  const lineSize = contentList.length - 1
-  const cbMeta: CodeBlockMeta = { langName: lang, lineSize, pre, code, div, contentList }
-  const { useContextMenu, showLangName, showLineNumber } = plugin.settings
-  // create copy button in right
-  addCopyButton(plugin, cbMeta)
+  const contentList: string[] = code.textContent.split(LINE_SPLIT_MARK);
+  const lineSize = contentList.length - 1;
+  const cbMeta: CodeBlockMeta = {
+    langName: lang,
+    lineSize,
+    pre,
+    code,
+    div,
+    contentList
+  };
+  const { useContextMenu, showLangName, showLineNumber } = plugin.settings;
   // create lang name tip in left
   if (showLangName) {
-    addLangName(plugin, cbMeta)
+    addLangName(plugin, cbMeta);
   }
   // add line number
   if (showLineNumber) {
-    addLineNumber(plugin, cbMeta)
+    addLineNumber(plugin, cbMeta);
   }
   //context menu
   if (useContextMenu) {
-    enhanceContextMenu(plugin, cbMeta)
+    enhanceContextMenu(plugin, cbMeta);
   }
 }
 
-
-
-function createElement (tagName: string, defaultClassName?: string) {
-  const element = document.createElement(tagName)
+function createElement(tagName: string, defaultClassName?: string) {
+  const element = document.createElement(tagName);
   if (defaultClassName) {
-    element.className = defaultClassName
+    element.className = defaultClassName;
   }
-  return element
+  return element;
 }
 
-function enhanceContextMenu (plugin: CodeBlockCopyPlugin, cbMeta: CodeBlockMeta) {
-  const { pre, code, lineSize } = cbMeta
+function enhanceContextMenu(plugin: CodeBlockEnhancer, cbMeta: CodeBlockMeta) {
+  const { pre, code, lineSize } = cbMeta;
   plugin.registerDomEvent(pre, 'contextmenu', (event) => {
-    event.preventDefault()
-    const target = <HTMLElement>event.target
+    event.preventDefault();
+    const target = <HTMLElement>event.target;
     if (target.tagName !== 'BUTTON') {
-      const contextMenu = new Menu(plugin.app)
-      const selection = window.getSelection().toString()
-      const editView = document.querySelector('.markdown-preview-view')
-      const blockHeight = parseFloat(window.getComputedStyle(pre).height)
-      const viewHeight = editView.clientHeight
-      const { offsetY, pageY } = event
-      const toBottom = blockHeight - ((viewHeight - pageY) + offsetY)
-      const toTop = offsetY - pageY + 100
-      const eventScrollTop = editView.scrollTop
-      let eRowNum = Math.ceil((offsetY - 16) / 24)
+      const contextMenu = new Menu(plugin.app);
+      const selection = window.getSelection().toString();
+      const editView = document.querySelector('.markdown-preview-view');
+      const blockHeight = parseFloat(window.getComputedStyle(pre).height);
+      const viewHeight = editView.clientHeight;
+      const { offsetY, pageY } = event;
+      const toBottom = blockHeight - (viewHeight - pageY + offsetY);
+      const toTop = offsetY - pageY + 100;
+      const eventScrollTop = editView.scrollTop;
+      let eRowNum = Math.ceil((offsetY - 16) / 24);
       if (eRowNum < 1) {
-        eRowNum = 1
+        eRowNum = 1;
       } else if (eRowNum > lineSize) {
-        eRowNum = lineSize
+        eRowNum = lineSize;
       }
       if (selection) {
         contextMenu.addItem((item) => {
@@ -121,104 +131,113 @@ function enhanceContextMenu (plugin: CodeBlockCopyPlugin, cbMeta: CodeBlockMeta)
             .setTitle('Copy')
             .setIcon('two-blank-pages', 16)
             .onClick((e) => {
-              navigator.clipboard.writeText(selection)
-            })
-        })
+              navigator.clipboard.writeText(selection);
+            });
+        });
       }
       contextMenu.addItem((item) => {
         item
           .setTitle('Copy Block')
           .setIcon('two-blank-pages', 16)
           .onClick(() => {
-            navigator.clipboard.writeText(code.textContent)
-          })
-      })
+            navigator.clipboard.writeText(code.textContent);
+          });
+      });
       contextMenu.addItem((item) => {
         item
           .setTitle('Copy Row')
           .setIcon('two-blank-pages', 16)
           .onClick(() => {
-            navigator.clipboard.writeText(cbMeta.contentList[eRowNum - 1])
-          })
-      })
+            navigator.clipboard.writeText(cbMeta.contentList[eRowNum - 1]);
+          });
+      });
 
       if (blockHeight > viewHeight) {
         if (toTop > 0) {
           contextMenu.addItem((item) => {
-            item
-              .setTitle('To Top')
-              .onClick((e) => {
-                editView.scrollTop = eventScrollTop - toTop
-              })
-          })
+            item.setTitle('To Top').onClick((e) => {
+              editView.scrollTop = eventScrollTop - toTop;
+            });
+          });
         }
         if (toBottom > 0) {
           contextMenu.addItem((item) => {
-            item
-              .setTitle('To Bottom')
-              .onClick((e) => {
-                editView.scrollTop = eventScrollTop + toBottom
-              })
-          })
+            item.setTitle('To Bottom').onClick((e) => {
+              editView.scrollTop = eventScrollTop + toBottom;
+            });
+          });
         }
       }
-      contextMenu.showAtPosition({ x: event.clientX, y: event.clientY })
+      contextMenu.showAtPosition({ x: event.clientX, y: event.clientY });
     }
-  })
-
+  });
 }
-function addLangName (plugin: CodeBlockCopyPlugin, cbMeta: CodeBlockMeta) {
-  const { langName, pre } = cbMeta
-  const langNameTip = createElement('span', 'code-block-lang-name')
-  langNameTip.innerText = langName
-  pre.appendChild(langNameTip)
+function addLangName(plugin: CodeBlockEnhancer, cbMeta: CodeBlockMeta) {
+  const { langName, pre } = cbMeta;
+  const langNameTip = createElement('span', 'code-block-lang-name');
+  langNameTip.innerText = langName;
+  pre.appendChild(langNameTip);
 }
 
-function addCopyButton (plugin: CodeBlockCopyPlugin, cbMeta: CodeBlockMeta) {
-  const { code, pre } = cbMeta
-  const copyButton = createElement('div', 'code-block-copy-button')
-  copyButton.setAttribute('aria-label', 'Copy')
-  replaceFirstChild(copyButton, SVG_COPY())
+/**
+ *
+ * 重新设置行元素的样式,这里主要设置高度
+ * @param singleLine 对应的行元素
+ * @param cbMeta 代码块数据
+ * @param i 行元素索引
+ */
+function resetLineNumberStyle(singleLine: HTMLElement, cbMeta: CodeBlockMeta, i: number) {
+  const tempSpan = document.createElement('span');
+  tempSpan.style.display = 'block';
+  tempSpan.innerText = cbMeta.contentList[i] || 'A';
+  cbMeta.code.appendChild(tempSpan);
+  const h = tempSpan.getBoundingClientRect().height;
+  if (h > 0) {
+    singleLine.style.height = h + 'px';
+  }
+  tempSpan && tempSpan.remove();
+}
 
-  const copyHandler = () => {
-    navigator.clipboard.writeText(code.textContent).then(() => {
-      replaceFirstChild(copyButton, SVG_SUCCESS())
-      setTimeout(() => {
-        replaceFirstChild(copyButton, SVG_COPY())
-      }, 1500);
-    }, () => {
-      copyButton.innerText = 'Error';
+/**
+ * 为代码块添加行号
+ * @param plugin 插件
+ * @param cbMeta 代码块数据
+ */
+function addLineNumber(plugin: CodeBlockEnhancer, cbMeta: CodeBlockMeta) {
+  const { pre } = cbMeta;
+  const lineNumber = createElement('span', 'code-block-linenum-wrap');
+  pre.appendChild(lineNumber);
+  pre.classList.add('code-block-pre__has-linenum');
+  setTimeout(() => {
+    Array.from({ length: cbMeta.lineSize }, (v, k) => k).forEach((i) => {
+      const singleLine = createElement('span', 'code-block-linenum');
+      lineNumber.appendChild(singleLine);
+      resetLineNumberStyle(singleLine, cbMeta, i);
     });
-  }
-  plugin.registerDomEvent(copyButton, 'click', copyHandler)
-  pre.appendChild(copyButton);
-  pre.classList.add('code-block-pre__has-copy-button')
+  }, 350);
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const linenumWrap = entry.target.parentElement.querySelector('.code-block-linenum-wrap');
+        const renderFlag =
+          linenumWrap.getBoundingClientRect().height < cbMeta.code.getBoundingClientRect().height;
+        if (linenumWrap && renderFlag) {
+          linenumWrap.querySelectorAll('span.code-block-linenum').forEach((line, index) => {
+            resetLineNumberStyle(line as HTMLElement, cbMeta, index);
+          });
+        }
+      }
+    });
+  });
+  observer.observe(cbMeta.code);
+  FILE_OBSERVER_CACHE.push(observer);
 }
 
-
-function addLineNumber (plugin: CodeBlockCopyPlugin, cbMeta: CodeBlockMeta) {
-  const { lineSize, pre } = cbMeta
-  // const { fontSize, lineHeight } = window.getComputedStyle(cbMeta.code)
-  const lineNumber = createElement('span', 'code-block-linenum-wrap')
-  Array.from({ length: lineSize }, (v, k) => k).forEach(i => {
-    const singleLine = createElement('span', 'code-block-linenum')
-    // singleLine.style.fontSize = fontSize
-    // singleLine.style.lineHeight = lineHeight
-    lineNumber.appendChild(singleLine)
-  })
-  pre.appendChild(lineNumber)
-  pre.classList.add('code-block-pre__has-linenum')
+export function clearObserverCache() {
+  FILE_OBSERVER_CACHE.length = 0;
 }
 
-function parseToSVG (svgString: string) {
-  return function () {
-    return new DOMParser().parseFromString(svgString, 'image/svg+xml').firstChild
-  }
-}
-
-function replaceFirstChild (target: HTMLElement, child: ChildNode) {
-  if (target.childNodes && target.childNodes.length > 0) {
-    target.removeChild(target.childNodes[0])
-  }
-  target.appendChild(child)
+export function disconnectObserver() {
+  FILE_OBSERVER_CACHE.forEach((ob) => ob && ob.disconnect());
 }
--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,99 @@
 import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { enhanceCodeBlock } from './core';
-import './styles/index.scss'
+import { disconnectObserver, enhanceCodeBlock } from './core';
+import './styles/index.scss';
 interface CbEnhancerSettings {
-	excludeLangs: string[];
-	showLangName: boolean;
-	showLineNumber: boolean;
-	useContextMenu: boolean;
+  excludeLangs: string[];
+  showLangName: boolean;
+  showLineNumber: boolean;
+  useContextMenu: boolean;
 }
 
 const DEFAULT_SETTINGS: CbEnhancerSettings = {
-	excludeLangs: ['todoist'],
-	showLangName: true,
-	showLineNumber: true,
-	useContextMenu: true
-}
+  excludeLangs: ['todoist'],
+  showLangName: true,
+  showLineNumber: true,
+  useContextMenu: true
+};
 
 export default class CodeBlockEnhancer extends Plugin {
-	settings: CbEnhancerSettings;
+  settings: CbEnhancerSettings;
 
-	async onload () {
-		await this.loadSettings();
-		this.addSettingTab(new CbEnhancerSettingsTab(this.app, this))
-		this.registerMarkdownPostProcessor((el, ctx) => {
-			enhanceCodeBlock(el, ctx, this)
-		})
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new CbEnhancerSettingsTab(this.app, this));
+    this.registerMarkdownPostProcessor((el, ctx) => {
+      enhanceCodeBlock(el, ctx, this);
+    });
+    console.log('Load Code Block Enhancer Plugin');
+  }
 
-	}
+  onunload() {
+    disconnectObserver();
+    console.log('Unloading Code Block Enhancer Plugin');
+  }
 
-	onunload () {
-		console.log('Unloading Code Block Enhancer Plugin');
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async loadSettings () {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings () {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-
 class CbEnhancerSettingsTab extends PluginSettingTab {
-	plugin: CodeBlockEnhancer;
+  plugin: CodeBlockEnhancer;
 
-	constructor(app: App, plugin: CodeBlockEnhancer) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: CodeBlockEnhancer) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display (): void {
-		let { containerEl } = this
-		const pluginSetting = this.plugin.settings
-		containerEl.empty()
-		containerEl.createEl('h2', { text: `Code Block Enhancer Settings ${this.plugin.manifest.version}` })
-		new Setting(containerEl)
-			.setName('Exclude language list')
-			.setDesc("Will not be enhanced in these languages")
-			.addTextArea(text => text
-				.setPlaceholder('Separate by `,` (like `todoist,other,...`)')
-				.setValue(pluginSetting.excludeLangs.join(','))
-				.onChange(async (value) => {
-					pluginSetting.excludeLangs = value.split(',');
-					await this.plugin.saveSettings();
-				}))
-		new Setting(containerEl)
-			.setName('Show language name')
-			.setDesc('Enable this options will show language name')
-			.addToggle(cb => {
-				cb
-					.setValue(pluginSetting.showLangName)
-					.onChange(async (isEnable) => {
-						pluginSetting.showLangName = isEnable
-						await this.plugin.saveSettings()
-					})
-			})
-		new Setting(containerEl)
-			.setName('Show line number')
-			.setDesc('Enable this options will show line number')
-			.addToggle(cb => {
-				cb
-					.setValue(pluginSetting.showLineNumber)
-					.onChange(async (isEnable) => {
-						pluginSetting.showLineNumber = isEnable
-						await this.plugin.saveSettings()
-					})
-			})
-		new Setting(containerEl)
-			.setName('Use ContextMenu')
-			.setDesc('Replace default contextmenu when right-click in code block')
-			.addToggle(cb => {
-				cb
-					.setValue(pluginSetting.useContextMenu)
-					.onChange(async (isEnable) => {
-						pluginSetting.useContextMenu = isEnable
-						await this.plugin.saveSettings()
-					})
-			})
-	}
+  display(): void {
+    let { containerEl } = this;
+    const pluginSetting = this.plugin.settings;
+    containerEl.empty();
+    containerEl.createEl('h2', {
+      text: `Code Block Enhancer Settings ${this.plugin.manifest.version}`
+    });
+    new Setting(containerEl)
+      .setName('Exclude language list')
+      .setDesc('Will not be enhanced in these languages')
+      .addTextArea((text) =>
+        text
+          .setPlaceholder('Separate by `,` (like `todoist,other,...`)')
+          .setValue(pluginSetting.excludeLangs.join(','))
+          .onChange(async (value) => {
+            pluginSetting.excludeLangs = value.split(',');
+            await this.plugin.saveSettings();
+          })
+      );
+    new Setting(containerEl)
+      .setName('Show language name')
+      .setDesc('Enable this options will show language name')
+      .addToggle((cb) => {
+        cb.setValue(pluginSetting.showLangName).onChange(async (isEnable) => {
+          pluginSetting.showLangName = isEnable;
+          await this.plugin.saveSettings();
+        });
+      });
+    new Setting(containerEl)
+      .setName('Show line number')
+      .setDesc('Enable this options will show line number')
+      .addToggle((cb) => {
+        cb.setValue(pluginSetting.showLineNumber).onChange(async (isEnable) => {
+          pluginSetting.showLineNumber = isEnable;
+          await this.plugin.saveSettings();
+        });
+      });
+    new Setting(containerEl)
+      .setName('Use ContextMenu')
+      .setDesc('Replace default contextmenu when right-click in code block')
+      .addToggle((cb) => {
+        cb.setValue(pluginSetting.useContextMenu).onChange(async (isEnable) => {
+          pluginSetting.useContextMenu = isEnable;
+          await this.plugin.saveSettings();
+        });
+      });
+  }
 }
